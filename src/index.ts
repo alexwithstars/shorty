be2ad5d0d6436c68d28bb1a1349dd8f286fb0577
alexwithstars <rayo080378@gmail.com>
2025-04-23 16:58:04 -0600

    fix(server): update static file serving path and improve error handling

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ import fs from 'node:fs/promises'
 import { ViteDevServer } from 'vite'
 import { BAD_REQUEST } from './utils/reponse.js'
 import debug from './debug.js'
+import { join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 console.log('Starting server...')
 
@@ -36,7 +38,7 @@ if (!PRODUCTION) {
   const compression = (await import('compression')).default
   const sirv = (await import('sirv')).default
   app.use(compression())
-  app.use(BASE_ROUTES.APP, sirv('./view', { extensions: [] }))
+  app.use(BASE_ROUTES.APP, sirv(join(fileURLToPath(import.meta.url), '..', '..', 'client'), { extensions: [] }))
 }
 
 // I'm aware of this, but in this case, it's not a problem
@@ -51,8 +53,12 @@ app.get(BASE_ROUTES.APP, async (req, res) => {
       template = await vite.transformIndexHtml(url, template)
       render = (await vite.ssrLoadModule('./src/view/entry-server.js')).render
     } else {
-      template = await fs.readFile('./index.html', 'utf-8')
-      render = (await import('./view/entry-server.js')).render
+      template = await fs.readFile('./dist/client/index.html', 'utf-8')
+      // this is expected 'cause this is for a production build and the file
+      // doesn't exist in the dev environment
+      // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
+      // @ts-ignore
+      render = (await import('../server/entry-server.js')).render
     }
 
     const rendered = render(url)
@@ -63,7 +69,7 @@ app.get(BASE_ROUTES.APP, async (req, res) => {
     res.status(200).set({ 'Content-Type': 'text/html' }).send(html)
   } catch (e: unknown) {
     if (vite === undefined || !(e instanceof Error)) {
-      res.status(500).end(e)
+      res.status(500).end(String(e))
       return
     }
     vite.ssrFixStacktrace(e)
